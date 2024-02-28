008ea68e901c28b3530e0856de05dd7b807f7f04
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-02-28 21:25:37 +0530

    Time: 15 ms (78.59%), Space: 11.7 MB (60.75%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -8,7 +8,6 @@ class Solution(object):
         result = 0
 
         for val in range(len(gain)):
-            print("cv :",current_value,"result: ", result)
             current_value = current_value + gain[val]
             result= max(current_value,result)
         return result  
