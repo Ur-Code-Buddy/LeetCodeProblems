92f489b6390d80db158dc6091cbf14f65ddc6396
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-03-03 14:21:28 +0530

    Time: 428 ms (66.05%), Space: 27.6 MB (42.51%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        print(len(set(nums)), len(nums) )
 
         if len(set(nums)) < len(nums):
             return True
