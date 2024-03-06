b7824f4a0e35f1fc30303d12012b80a5d15d0ee6
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-03-06 22:43:32 +0530

    Time: 29 ms (65.70%), Space: 12.4 MB (87.63%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,8 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        string= ""
-        for letters in s:
-            if letters.isalnum():
-                string += letters.lower()
-        return string == string[::-1]
+        left = 0
+        right = len(s) - 1
+        while (left < right):
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            else:
+                left += 1
+                right -= 1
+        return True
+
