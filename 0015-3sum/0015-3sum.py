442c443e8ee45ded79c53d1ee8cee62d1b6d747f
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-03-21 18:54:58 +0530

    Time: 877 ms (58.31%), Space: 14.9 MB (88.83%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,30 +5,34 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        n = len(nums)
+        length = len(nums)
         res = []
         
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i in range(length):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
-            l = i + 1
-            r = n - 1
-            
-            while l < r:
-                total = nums[i] + nums[l] + nums[r]
                 
+            l = i + 1
+            r = length - 1
+            
+            while (l < r):
+                total = nums[i] + nums[l] + nums[r]
                 if total == 0:
                     res.append([nums[i], nums[l], nums[r]])
+                    
                     while l < r and nums[l] == nums[l + 1]:
-                        l += 1  
-                    while l < r and nums[r] == nums[r - 1]:
-                        r -= 1  
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                        
                     l += 1
                     r -= 1
+                    
                 elif total < 0:
                     l += 1
                 else:
                     r -= 1
-        
+                    
         return res
+                        
+        
