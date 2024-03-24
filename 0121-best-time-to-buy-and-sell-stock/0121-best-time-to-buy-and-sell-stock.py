ac2b1cd0300d328acb95ad8f65729665b862a49b
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-03-24 19:11:40 +0530

    Time: 756 ms (72.91%), Space: 21.8 MB (5.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -6,10 +6,13 @@ class Solution(object):
         """
         l, r = 0, 1
         maxP = 0
+        length = len(prices)
         
-        while r < len(prices) :
+        while r < length:
             if prices[l] < prices[r]:
-                maxP = max(maxP, prices[r] - prices[l])
+                if prices[r] - prices[l] > maxP:
+                    maxP = prices[r] - prices[l]
+                #maxP = max(maxP, prices[r] - prices[l])
             else: l = r
                 
             r += 1
