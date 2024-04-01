995da8e6a763f285df5e4df31a1279299114f6da
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-04-01 08:07:25 +0530

    Time: 20 ms (11.10%), Space: 11.8 MB (83.93%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -8,12 +8,14 @@ class Solution(object):
             return 0
         
         end = len(s) - 1
+        
         while end >= 0 and s[end] == ' ':
             end -= 1
         
         length = 0
-        while end >= 0 and s[end] != ' ':
+        while end >= 0 and s[end] != " ":
             length += 1
             end -= 1
-        
+            
         return length
+        
