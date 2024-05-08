7d3b75bce57c6c5abebc256a18c23f600685d92f
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-05-08 10:17:05 +0530

    Time: 56 ms (79.63%), Space: 51.3 MB (31.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,8 +7,7 @@ var twoSum = function(nums, target) {
     let hashmap = {};
     let index = 0;
     for (val of nums){
-        let temp = target - val;
-        if(temp in hashmap){
+        if(target - val in hashmap){
             return [index, hashmap[target-val] ];
         }
         else{
