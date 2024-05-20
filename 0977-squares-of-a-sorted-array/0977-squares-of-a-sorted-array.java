b05d5eb78c0e449bdb108eb18b2af3f3379e7d03
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-05-20 21:55:21 +0530

    Time: 1 ms (100.00%), Space: 47.6 MB (7.05%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -4,9 +4,30 @@ class Solution {
         int n = nums.length; 
         int[] res = new int[n];
         
-        for (int i = 0; i < n; i ++)
-            res[i] = nums[i] * nums[i];
-        Arrays.sort(res);
+        // for (int i = 0; i < n; i ++)
+        //     res[i] = nums[i] * nums[i];
+        // Arrays.sort(res);
+        
+        int ind = n - 1;
+        int l = 0; 
+        int r = n - 1;
+        
+        while (l <= r)
+        {
+            int sqOfLeft = nums[l] * nums[l];
+            int sqOfRight = nums[r] * nums[r];
+            if (sqOfRight > sqOfLeft)
+            {
+                res[ind] = sqOfRight;
+                r --;
+            }
+            else
+            {
+                res[ind] = sqOfLeft;
+                l ++;
+            }
+            ind --;
+        }
         
         return res;
     }
