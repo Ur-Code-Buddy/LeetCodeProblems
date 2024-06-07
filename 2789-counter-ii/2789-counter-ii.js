d452aacde222697bf5f29adbb75eaeb92d11dd5b
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-06-07 14:39:10 +0530

    Time: 65 ms (23.49%), Space: 51.8 MB (32.94%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+    return {
+        increment(){
+            count++;
+            return count;
+        },
+        decrement(){
+            count--;
+            return count;
+        },
+        reset(){
+            return (count = init);
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
