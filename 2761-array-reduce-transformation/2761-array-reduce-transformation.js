7ea475a794f791f31ca40ffd4afc16412121710f
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-06-08 17:38:16 +0530

    Time: 59 ms (26.78%), Space: 49.5 MB (21.73%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.js
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -7,11 +7,9 @@
 var reduce = function(nums, fn, init) {
     let temp_val = fn(init, nums[0]);
     if (nums.length == 0) {return init;}
-    console.log(`temp_val: ${temp_val}`);
 
     for (let i = 1; i < nums.length; i ++){
         temp_val = fn(temp_val, nums[i]);
-        console.log(temp_val);
     }
     return temp_val;    
 };
