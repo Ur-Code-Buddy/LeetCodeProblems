a2efc9482620cb2807b86acf3ec8c3ced56fcca0
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-06-21 12:50:23 +0530

    Time: 14 ms (63.59%), Space: 11.5 MB (91.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,14 +1,13 @@
 class Solution(object):
     def rob(self, nums):
-        memo = {}
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        rob1, rob2 = 0,0
 
-        def _rob_helper(i):
-            if i < 0:
-                return 0
-            if i in memo:
-                return memo[i]
-            result = max(_rob_helper(i - 2) + nums[i], _rob_helper(i - 1))
-            memo[i] = result
-            return result
-
-        return _rob_helper(len(nums) - 1)
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp 
+        return rob2
