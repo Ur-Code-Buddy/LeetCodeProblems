bceb1ee07a40728931da723a68711eaa92384ce4
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-06-21 12:39:11 +0530

    Time: 16 ms (49.2%), Space: 11.9 MB (10.3%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,13 +2,13 @@ class Solution(object):
     def rob(self, nums):
         memo = {}
 
-        def _rob(i):
+        def rob_helper(i):
             if i < 0:
                 return 0
             if i in memo:
                 return memo[i]
-            result = max(_rob(i - 2) + nums[i], _rob(i - 1))
+            result = max(rob_helper(i - 2) + nums[i], rob_helper(i - 1))
             memo[i] = result
             return result
 
-        return _rob(len(nums) - 1)
+        return rob_helper(len(nums) - 1)
