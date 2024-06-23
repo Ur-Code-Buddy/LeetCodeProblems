159a96aa9566e1bc2ffceff7d802ab360727e050
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-06-23 12:03:34 +0530

    Time: 89 ms (86.87%), Space: 12.6 MB (98.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,9 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        hashset = set(nums)
-        n = len(nums)
-        for i in range(n + 1):
-            if i not in hashset:
-                return i
+        # hashset = set(nums)
+        # n = len(nums)
+        # for i in range(n + 1):
+        #     if i not in hashset:
+        #         return i
         
+
+        n = len(nums)
+        total_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        return total_sum - actual_sum
