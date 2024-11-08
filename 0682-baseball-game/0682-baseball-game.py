b7c6f72c65405bfc02bf3757141ec3ebf258e1c0
Baivab <05baivab@gmail.com>
2024-11-08 20:00:14 +0530

    Time: 3 ms (50.24%), Space: 11.9 MB (75.24%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def calPoints(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        stack = []
+
+        for i in operations:
+            if i == "+":
+                stack.append(stack[-1] + stack[-2]) 
+            elif i == "D": 
+                stack.append(stack[-1] * 2 )
+            elif i == "C":
+                stack.pop() 
+            else:
+                stack.append(int(i) )
+        res = 0
+        for i in stack:
+            res = res + i
+        return res
