7a689492e1996ffe30d80c95f5c747a18712fc16
Baivab <05baivab@gmail.com>
2024-12-30 11:01:41 +0530

    Time: 16 ms (26.99%), Space: 19.8 MB (6.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,17 +6,19 @@ class Solution(object):
         :rtype: List[int]
         """
         count = {}
-        arr = [[] for i in range(len(nums) + 1 ) ]
+        arr = [[] for i in range(1 + len(nums))]
 
         for val in nums:
-            count[val] = count.get(val, 0) + 1
+            count[val] = count.get(val,0) + 1 #> {1:3, 2:2, 3:1}
         
         res = []
         for n, c in count.items():
             arr[c].append(n)
         
-        for i in range(len(arr)-1, 0, -1):
-            for j in arr[i]: #every array can have more subelements
+        for i in range(len(arr) - 1, 0 , -1):
+            for j in arr[i]:
                 res.append(j)
-            if len(res) == k:
-                return res
+                if len(res) == k:
+                    return res
+
+        
