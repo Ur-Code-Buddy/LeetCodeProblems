45f01367da979f0f4a2d2af4ed7f661842aae0f5
Baivab <05baivab@gmail.com>
2025-01-07 13:03:11 +0530

    Time: 14 ms (40.3%), Space: 20 MB (6.2%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,20 +5,24 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        
         count = {}
-        arr = [[] for i in range(1 + len(nums))]
+        arr = [ [] for i in range(1+len(nums))]
 
         for val in nums:
-            count[val] = count.get(val,0) + 1 #> {1:3, 2:2, 3:1}
+            count[val] = count.get(val,0) + 1
         
         res = []
         for n, c in count.items():
             arr[c].append(n)
         
-        for i in range(len(arr) - 1, 0 , -1):
+
+        for i in range(len(arr) - 1, 0, -1):
             for j in arr[i]:
                 res.append(j)
                 if len(res) == k:
                     return res
+
+        
 
         
